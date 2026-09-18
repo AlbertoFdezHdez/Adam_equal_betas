e528bee40a9f82de19ac0544c5a3764e809a0637
AlbertoFdezHdez <a.fernandez@upv.es>
2026-09-18 15:52:50 +0200

    again

--- analysis/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Analysis entrypoints for the manuscript experiments."""
--- code/utils/models/efficientnet.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-import torchvision
-
-def load_efficientnet(input_shape, num_classes):
-    model = torchvision.models.efficientnet_v2_s()
-    model.classifier[-1] = nn.Linear(1280, num_classes, bias=True)
-    return model
